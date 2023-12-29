dde0da9dfd21a4ea1230bbbf2b47d87048cf3414
Tiago Pimenta <tiago.pimenta@ucsal.edu.br>
2023-12-29 14:41:21 -0300

    criação do Carrinho de compras!

---
+++ src/main/java/list/OperacoesBasicas/CarrinhoDeCompras.java
@@ -0,0 +1,81 @@
+package main.java.list.OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+
+    private List<Item> itemList;
+
+    public CarrinhoDeCompras() {
+        this.itemList = new ArrayList<>();
+    }
+
+    public void adicionarItem(String nome, double preco, int quantidade) {
+        Item item = new Item(nome, preco, quantidade);
+        this.itemList.add(item);
+    }
+
+    public void removerItem(String nome) {
+        List<Item> itensParaRemover = new ArrayList<>();
+
+        if (!itemList.isEmpty()) {
+            for (Item i : itemList) {
+                if (i.getNome().equalsIgnoreCase(nome)) {
+                    itensParaRemover.add(i);
+                }
+            }
+            itemList.removeAll(itensParaRemover);
+        } else {
+            System.out.println("A lista está vazia!");
+        }
+    }
+
+    public double calcularValorTotal() {
+        double valorTotal = 0d;
+
+        if (!itemList.isEmpty()) {
+            for (Item item : itemList) {
+                double valorItem = item.getPreco() * item.getQuant();
+                valorTotal += valorItem;
+            }
+            return valorTotal;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    public void exibirItens() {
+        if (!itemList.isEmpty()) {
+            System.out.println(this.itemList);
+        } else {
+            System.out.println("A lista está vazia!");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "CarrinhoDeCompras{" +
+                "itemList=" + itemList +
+                '}';
+    }
+
+    public static void main(String[] args) {
+
+        CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
+
+        carrinhoDeCompras.adicionarItem("caderno", 27.5, 2);
+        carrinhoDeCompras.adicionarItem("classificador", 10.45, 4);
+        carrinhoDeCompras.adicionarItem("lapiseira", 14.45, 6);
+
+
+        carrinhoDeCompras.removerItem("lapiseira");
+
+        carrinhoDeCompras.adicionarItem("lapiseira", 14.45, 6);
+
+        System.out.println("O valor total da compra é: " + carrinhoDeCompras.calcularValorTotal());
+
+
+    }
+}
+
